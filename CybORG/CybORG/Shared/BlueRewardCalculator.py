d55f3da9e966a1f0c1aa11afa5c3473c9acf5305
npotteig <npottei1@umbc.edu>
2024-04-10 13:41:23 -0500

    bug fix with strat switch agent

--- CybORG/CybORG/Shared/BlueRewardCalculator.py
+++ CybORG/CybORG/Shared/BlueRewardCalculator.py
@@ -11,22 +11,29 @@ class ConfidentialityRewardCalculator(RewardCalculator):
     # Calculate punishment for defending agent based on compromise of hosts/data
     def __init__(self, agent_name: str, scenario: Scenario):
         self.scenario = scenario
-        self.adversary = scenario.get_agent_info(agent_name).adversary
+        # self.adversary = scenario.get_agent_info(agent_name).adversary
+        self.adversary_1 = 'Red'
+        self.adversary_2 = 'Red2'
         super(ConfidentialityRewardCalculator, self).__init__(agent_name)
-        self.infiltrate_rc = PwnRewardCalculator(self.adversary, scenario)
+        self.infiltrate_rc_1 = PwnRewardCalculator(self.adversary_1, scenario)
+        self.infiltrate_rc_2 = PwnRewardCalculator(self.adversary_2, scenario)
         self.compromised_hosts = {}
 
     def reset(self):
-        self.infiltrate_rc.reset()
+        self.infiltrate_rc_1.reset()
+        self.infiltrate_rc_2.reset()
 
     def calculate_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
         self.compromised_hosts = {}
-        reward = -self.infiltrate_rc.calculate_reward(current_state, action, agent_observations, done)
+        reward_1 = -self.infiltrate_rc_1.calculate_reward(current_state, action, agent_observations, done)
+        reward_2 = -self.infiltrate_rc_2.calculate_reward(current_state, action, agent_observations, done)
         self._calculate_compromised_hosts()
-        return reward
+        return reward_1 + reward_2
 
     def _calculate_compromised_hosts(self):
-        for host, value in self.infiltrate_rc.compromised_hosts.items():
+        for host, value in self.infiltrate_rc_1.compromised_hosts.items():
+            self.compromised_hosts[host] = -1 * value
+        for host, value in self.infiltrate_rc_2.compromised_hosts.items():
             self.compromised_hosts[host] = -1 * value
 
 
@@ -34,21 +41,28 @@ class AvailabilityRewardCalculator(RewardCalculator):
     # Calculate punishment for defending agent based on reduction in availability
     def __init__(self, agent_name: str, scenario: Scenario):
         super(AvailabilityRewardCalculator, self).__init__(agent_name)
-        self.adversary = scenario.get_agent_info(agent_name).adversary
-        self.disrupt_rc = DistruptRewardCalculator(self.adversary, scenario)
+        # self.adversary = scenario.get_agent_info(agent_name).adversary
+        self.adversary_1 = 'Red'
+        self.adversary_2 = 'Red2'
+        self.disrupt_rc_1 = DistruptRewardCalculator(self.adversary_1, scenario)
+        self.disrupt_rc_2 = DistruptRewardCalculator(self.adversary_2, scenario)
         self.impacted_hosts = {}
 
     def reset(self):
-        self.disrupt_rc.reset()
+        self.disrupt_rc_1.reset()
+        self.disrupt_rc_2.reset()
 
     def calculate_reward(self, current_state: dict, action: dict, agent_observations: dict, done: bool) -> float:
         self.impacted_hosts = {}
-        reward = -self.disrupt_rc.calculate_reward(current_state, action, agent_observations, done)
+        reward_1 = -self.disrupt_rc_1.calculate_reward(current_state, action, agent_observations, done)
+        reward_2 = -self.disrupt_rc_2.calculate_reward(current_state, action, agent_observations, done)
         self._calculate_impacted_hosts()
-        return reward
+        return reward_1 + reward_2
 
     def _calculate_impacted_hosts(self):
-        for host, value in self.disrupt_rc.impacted_hosts.items():
+        for host, value in self.disrupt_rc_1.impacted_hosts.items():
+            self.impacted_hosts[host] = -1 * value
+        for host, value in self.disrupt_rc_2.impacted_hosts.items():
             self.impacted_hosts[host] = -1 * value
 
 class HybridAvailabilityConfidentialityRewardCalculator(RewardCalculator):
